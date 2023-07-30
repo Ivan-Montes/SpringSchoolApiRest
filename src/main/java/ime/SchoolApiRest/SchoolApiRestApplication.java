f614cffddad17240f9a7c32e269af7f0182192a1
IvanM <ivan.montes@outlook.com>
2023-07-30 11:56:18 +0200

    Init commit

---
+++ src/main/java/ime/SchoolApiRest/SchoolApiRestApplication.java
@@ -0,0 +1,13 @@
+package ime.SchoolApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ime/SchoolApiRest/SchoolApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ime.SchoolApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
