239d5dd9101cd5956148baea54751f2f9e652b06
IvanM <ivan.montes@outlook.com>
2023-08-02 17:01:34 +0200

    Add service layer

---
+++ src/main/java/ime/SchoolApiRest/service/SubjectService.java
@@ -0,0 +1,7 @@
+package ime.SchoolApiRest.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SubjectService {
+
+}
---
+++ src/main/java/ime/SchoolApiRest/service/TeacherService.java
@@ -0,0 +1,8 @@
+package ime.SchoolApiRest.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface TeacherService {
+
+}
