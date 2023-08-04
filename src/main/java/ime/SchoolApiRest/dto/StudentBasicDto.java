98a96418639ec9837ba21a076e88bb45587fd80a
IvanM <ivan.montes@outlook.com>
2023-08-04 12:13:14 +0200

    Add Dto with basic data

--- src/main/java/ime/SchoolApiRest/dto/StudentBasicDto.java
+++ src/main/java/ime/SchoolApiRest/dto/StudentBasicDto.java
@@ -2,30 +2,19 @@ package ime.SchoolApiRest.dto;
 
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
-
-import java.util.Set;
-
-import ime.SchoolApiRest.entity.Teacher;
-
-import java.util.HashSet;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
-public class SubjectDto{
+@Getter
+@Setter
+public class StudentBasicDto {
 
-	private Long subjectId;
+	private Long studentId;
 	
 	@Size(min = 1, max = 50)
 	private String name;
-
-	private Teacher teacher;
-	
-	private Set<StudentDto>students = new HashSet<>();
 	
 }
--- src/main/java/ime/SchoolApiRest/dto/SubjectBasicDto.java
+++ src/main/java/ime/SchoolApiRest/dto/SubjectBasicDto.java
@@ -1,31 +1,21 @@
 package ime.SchoolApiRest.dto;
 
+
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-import java.util.Set;
-
-import ime.SchoolApiRest.entity.Teacher;
-
-import java.util.HashSet;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class SubjectDto{
+public class SubjectBasicDto {	
 
 	private Long subjectId;
 	
 	@Size(min = 1, max = 50)
 	private String name;
 
-	private Teacher teacher;
-	
-	private Set<StudentDto>students = new HashSet<>();
-	
+
 }
