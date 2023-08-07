5a01949af22bf8749f8bd5d911636f3b26172034
IvanM <ivan.montes@outlook.com>
2023-08-07 11:38:18 +0200

    Add new custom exception

--- src/main/java/ime/SchoolApiRest/exception/GlobalExceptionHandler.java
+++ src/main/java/ime/SchoolApiRest/exception/GlobalExceptionHandler.java
@@ -8,8 +8,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+	private final String simpleText = " _***- Identifier: ";
+	
 	@ExceptionHandler(ime.SchoolApiRest.exception.ResourceNotFoundException.class)
-	public ResponseEntity<String> ResourceNotFoundException(ResourceNotFoundException ex) {
-		return new ResponseEntity<String>(ex.getMessage() + " _***- Identifier: " + ex.getIdentifier(), HttpStatus.NOT_FOUND);
+	public ResponseEntity<String> resourceNotFoundException(ResourceNotFoundException ex) {
+		return new ResponseEntity<String>(ex.getMessage() + simpleText + ex.getIdentifier(), HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(ime.SchoolApiRest.exception.SubjectAssociatedException.class)
+	public ResponseEntity<String> subjectAssociatedException(SubjectAssociatedException ex){
+		return new ResponseEntity<String>(ex.getMessage() + simpleText + ex.getIdentifier(),HttpStatus.PRECONDITION_REQUIRED);
 	}
 }
