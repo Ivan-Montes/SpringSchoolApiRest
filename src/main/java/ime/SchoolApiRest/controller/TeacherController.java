a5c0cb18e7a3619051206e1a9085d8b9408b7315
IvanM <ivan.montes@outlook.com>
2023-08-11 10:34:25 +0200

    Add JavaDoc annotations to TeacherController

--- src/main/java/ime/SchoolApiRest/controller/TeacherController.java
+++ src/main/java/ime/SchoolApiRest/controller/TeacherController.java
@@ -18,7 +18,12 @@ import ime.SchoolApiRest.dto.TeacherBasicCreationDto;
 import ime.SchoolApiRest.dto.TeacherBasicDto;
 import ime.SchoolApiRest.dto.TeacherDto;
 import ime.SchoolApiRest.service.TeacherService;
-
+/**
+ * Controller for Teacher environment
+ * 
+ * @author Ivan-Montes
+ *
+ */
 @RestController
 @RequestMapping("/api/teachers")
 public class TeacherController {
@@ -26,13 +31,23 @@ public class TeacherController {
 	@Autowired
 	private TeacherService teacherService;
 	
+	/**
+	 * Get a List of all teachers
+	 * 
+	 * @return ResponseEntity with a List of TeacherDto
+	 */
 	@GetMapping
 	public ResponseEntity<List<TeacherDto>> getAllEagerTeachersDto(){
 		
 		return ResponseEntity.ok(teacherService.getAllEagerTeachersDto());
 		
 	}
-	
+	/**
+	 * Get a teacher according to Id
+	 * 
+	 * @param id with the identifier of a teacher
+	 * @return ResponseEntity with the teacher required
+	 */
 	@GetMapping("{id}")
 	public ResponseEntity<TeacherBasicDto>getTeacherDtoById(@PathVariable Long id){
 		
@@ -40,29 +55,61 @@ public class TeacherController {
 		
 	}
 	
+	/**
+	 * Delete a teacher by its Id
+	 * 
+	 * @param id with the identifier of a teacher
+	 * @return ResponseEntity with a message
+	 */
 	@DeleteMapping("{id}")
 	public ResponseEntity<String>deteteTeacher(@PathVariable Long id){
 		teacherService.deleteTeacherById(id);
 		return ResponseEntity.ok("Teacher with identifier " + id + " deleted successfully");
 	}
 	
+	/**
+	 * Create a new teacher
+	 * 
+	 * @param tbcDto. Object TeacherBasicCreationDto for adding in the DB
+	 * @return ResponseEntity with the teacher required
+	 */
 	@PostMapping
 	public ResponseEntity<TeacherBasicDto>createTeacher(@RequestBody TeacherBasicCreationDto tbcDto){
 		TeacherBasicDto tbd = teacherService.createTeacher(tbcDto);
 		return new ResponseEntity<TeacherBasicDto>(tbd, HttpStatus.CREATED);
 	}
 	
+	/**
+	 * Update fields in a teacher
+	 * 
+	 * @param tbd Object TeacherBasicCreationDto with the new info
+	 * @return ResponseEntity with the teacher modified
+	 */
 	@PutMapping
 	public ResponseEntity<TeacherBasicDto>updateTeacher(@RequestBody TeacherBasicDto tbd){
 		return ResponseEntity.ok(teacherService.updateTeacher(tbd.getTeacherId(), tbd));
 	}
 	
+	/**
+	 * Add a subject in a teacher
+	 * 
+	 * @param teacherId identifier of a teacher
+	 * @param subjectId identifier of a subject
+	 * @return ResponseEntity with the teacher modified
+	 */
 	@GetMapping("{teacherId}/{subjectId}")
 	public ResponseEntity<TeacherDto>addSubject(@PathVariable Long teacherId, @PathVariable Long subjectId){
 		TeacherDto teacherDto = teacherService.addSubjectToTeacher(teacherId, subjectId);
 		return ResponseEntity.ok(teacherDto);
 	}
 	
+	/**
+	 * Remove a subject from a teacher
+	 * 
+	 * @param teacherId identifier of a teacher
+	 * @param subjectId identifier of a subject
+	 * @return ResponseEntity with the teacher modified
+	 */
 	@DeleteMapping("{teacherId}/{subjectId}")
 	public ResponseEntity<TeacherDto>removeSubject(@PathVariable Long teacherId, @PathVariable Long subjectId){
 		TeacherDto teacherDto = teacherService.removeSubjectFromTeacher(teacherId, subjectId);
