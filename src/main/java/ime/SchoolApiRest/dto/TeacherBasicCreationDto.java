425b1207ee3ca0a691147388a87e49361ae503cd
IvanM <ivan.montes@outlook.com>
2023-08-12 13:29:55 +0200

    Add message for Size validation

--- src/main/java/ime/SchoolApiRest/dto/TeacherBasicCreationDto.java
+++ src/main/java/ime/SchoolApiRest/dto/TeacherBasicCreationDto.java
@@ -15,11 +15,11 @@ import lombok.Setter;
 @Builder
 public class TeacherBasicCreationDto {
 
-	@Size(min = 1, max = 50)
+	@Size(min = 1, max = 50, message="{Size.Teacher.name}")
 	@Pattern( regexp = "[a-zA-Z\\s\\-&]+", message="{Pattern.Teacher.name}")
 	private String name;
 	
-	@Size(min = 1, max = 50)
+	@Size(min = 1, max = 50, message="{Size.Teacher.surname}")
 	@Pattern( regexp = "[a-zA-Z\\s\\-&]+", message="{Pattern.Teacher.surname}")
 	private String surname;
 }
