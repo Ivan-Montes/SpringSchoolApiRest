063ba34ed226203065d09d3d2e8b67fa95e69a24
IvanM <ivan.montes@outlook.com>
2023-08-14 19:02:32 +0200

    JUnit in Subject

--- src/test/java/ime/SchoolApiRest/repository/SubjectRepositoryTest.java
+++ src/test/java/ime/SchoolApiRest/repository/SubjectRepositoryTest.java
@@ -1,14 +1,28 @@
 package ime.SchoolApiRest.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ime.SchoolApiRest.entity.Subject;
+import java.util.List;
 
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class SubjectRepositoryTest {
 
+	@Autowired
+	private SubjectRepository subjectRepo;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void SubjectRepository_getAllEagerSubject_ReturnListSubjectDto() {
+		List<Subject> subjects = subjectRepo.getAllEagerSubject();
+		Assertions.assertThat(subjects).isNotNull();
+		Assertions.assertThat(subjects).hasSizeGreaterThanOrEqualTo(0);
 	}
 
 }
--- src/test/java/ime/SchoolApiRest/service/impl/SubjectStudentServiceImplTest.java
+++ src/test/java/ime/SchoolApiRest/service/impl/SubjectStudentServiceImplTest.java
@@ -1,14 +1,67 @@
 package ime.SchoolApiRest.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import ime.SchoolApiRest.entity.Subject;
+import ime.SchoolApiRest.repository.SubjectRepository;
+import ime.SchoolApiRest.dto.*;
+
+@ExtendWith(MockitoExtension.class)
 class SubjectStudentServiceImplTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@Mock
+	private SubjectRepository subjectRepo;
+	
+	
+	@InjectMocks
+	private SubjectServiceImpl subjectService;
+	
+	private Subject subjectTest;
+	
+	@BeforeEach
+	private void SubjectServiceImpl_createUsers() {
+		subjectTest = new Subject();
+		subjectTest.setSubjectId(1L);
+		subjectTest.setName("101");
+		subjectTest.setTeacher(null);
+		subjectTest.setStudents(new HashSet<>());
+		
 	}
+	
+	
+	@Test
+	public void subjectStudentServiceImpl_getAllEagerSubjectDto_ReturSetSubjectDto() {
+		
+		List<Subject>subjects = List.of(subjectTest);
+		doReturn(subjects).when(subjectRepo).getAllEagerSubject();
+		
+		Set<SubjectDto>subjectList = subjectService.getAllEagerSubjectDto();
+		
+		assertAll(
+					()->Assertions.assertThat(subjectList).isNotNull(),
+					()->Assertions.assertThat(subjectList).hasSizeGreaterThanOrEqualTo(0)
+				);		
+		verify(subjectRepo,times(1)).getAllEagerSubject();
+		
+	}
+	
+	
+	
 
 }
