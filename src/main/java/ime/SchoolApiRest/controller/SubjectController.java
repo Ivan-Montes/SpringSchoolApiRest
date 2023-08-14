443d0c46d3dc4f4b625b238ffce8d35728261b63
IvanM <ivan.montes@outlook.com>
2023-08-14 18:20:23 +0200

    Add method to retrieve a collection of Subjects

--- src/main/java/ime/SchoolApiRest/controller/SubjectController.java
+++ src/main/java/ime/SchoolApiRest/controller/SubjectController.java
@@ -1,7 +1,32 @@
 package ime.SchoolApiRest.controller;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ime.SchoolApiRest.dto.SubjectDto;
+import ime.SchoolApiRest.service.SubjectService;
+
 @RestController
+@RequestMapping("/api/subjects")
 public class SubjectController {
 
+	@Autowired
+	private SubjectService subjectService;
+	
+	/**
+	 * Get a List of all subjects
+	 * 
+	 * @return ResponseEntity with a List of SubjectDto
+	 */
+	@GetMapping
+	public ResponseEntity<Set<SubjectDto>> getAllEagerSubjectDto(){
+		return ResponseEntity.ok(subjectService.getAllEagerSubjectDto());
+	}
+	
+	
+	
 }
