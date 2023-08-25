eb6320b3b5b5bb899762edb7358c1ee7bffe4073
IvanM <ivan.montes@outlook.com>
2023-08-25 13:31:43 +0200

    Configure oauth2 with github

--- src/main/java/ime/SchoolApiRest/security/SecurityConfig.java
+++ src/main/java/ime/SchoolApiRest/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package ime.SchoolApiRest.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,6 +33,7 @@ public class SecurityConfig {
                 	auth.anyRequest().authenticated();
                 })
                 .headers(head-> head.frameOptions(f->f.sameOrigin())) // H2 database
+                .oauth2Login(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .build();	
