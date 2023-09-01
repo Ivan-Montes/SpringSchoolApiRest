40ee47a95b8996dae50bbe7d98c9502c7e5bedd7
IvanM <ivan.montes@outlook.com>
2023-09-01 19:36:52 +0200

    Add method create

--- src/main/java/ime/SchoolApiRest/service/impl/SubjectStudentServiceImpl.java
+++ src/main/java/ime/SchoolApiRest/service/impl/SubjectStudentServiceImpl.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Service;
 import ime.SchoolApiRest.dto.SubjectStudentCreationDto;
 import ime.SchoolApiRest.dto.SubjectStudentCuteDto;
 import ime.SchoolApiRest.entity.SubjectStudentId;
+import ime.SchoolApiRest.entity.Student;
+import ime.SchoolApiRest.entity.Subject;
 import ime.SchoolApiRest.entity.SubjectStudent;
 import ime.SchoolApiRest.exception.ResourceNotFoundException;
 import ime.SchoolApiRest.mapper.SubjectStudentMapper;
+import ime.SchoolApiRest.repository.StudentRepository;
+import ime.SchoolApiRest.repository.SubjectRepository;
 import ime.SchoolApiRest.repository.SubjectStudentRepository;
 import ime.SchoolApiRest.service.SubjectStudentService;
 
@@ -19,6 +23,12 @@ public class SubjectStudentServiceImpl implements SubjectStudentService {
 
 	@Autowired
 	private SubjectStudentRepository subjectStudentRepo;
+	
+	@Autowired
+	private SubjectRepository subjectRepo;
+	
+	@Autowired
+	private StudentRepository studentRepo;
 	
 	@Override
 	public List<SubjectStudentCuteDto> getAllSubjectStudent() {
@@ -42,8 +52,14 @@ public class SubjectStudentServiceImpl implements SubjectStudentService {
 
 	@Override
 	public SubjectStudentCuteDto createSubjectStudent(SubjectStudentCreationDto sscDto) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Subject subject = subjectRepo.findById(sscDto.getSubjectId()).orElseThrow( () -> new ResourceNotFoundException(sscDto.getSubjectId()));
+		Student student = studentRepo.findById(sscDto.getStudentId()).orElseThrow( () -> new ResourceNotFoundException(sscDto.getStudentId()));
+		SubjectStudentId ssId = new SubjectStudentId(subject.getSubjectId(), student.getStudentId());
+		SubjectStudent ss = new SubjectStudent(ssId,subject,student,sscDto.getAverageGrade());
+		
+		return SubjectStudentMapper.toSubjectStudentCuteDto(subjectStudentRepo.save(ss));
+		
 	}
 
 	@Override
