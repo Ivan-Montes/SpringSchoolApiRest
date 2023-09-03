2b0b337261e9193aa353596703420328dd041976
IvanM <ivan.montes@outlook.com>
2023-09-03 11:04:41 +0200

    Add delete method in controller

--- src/main/java/ime/SchoolApiRest/controller/SubjectStudentController.java
+++ src/main/java/ime/SchoolApiRest/controller/SubjectStudentController.java
@@ -1,11 +1,14 @@
 package ime.SchoolApiRest.controller;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -65,4 +68,39 @@ public class SubjectStudentController {
 				
 	}
 
+	/**
+	 * 
+	 *  Delete a deleteSubjectStudent by Id
+	 * 
+	 * @param request Composite key
+	 * @return ResponseEntity with message
+	 */
+	@DeleteMapping
+	public ResponseEntity<String>deleteSubjectStudentById(@RequestBody Map<String, String> request){
+		
+		String studentId = request.get("studentId");
+		String subjectId = request.get("subjectId");
+		
+		if( !studentId.matches("\\d+") || !subjectId.matches("\\d+")) {
+			
+			return new ResponseEntity<>("Error in composite key", HttpStatus.BAD_REQUEST);
+		}
+		
+		subjectStudentService.deleteSubjectStudentById(Long.parseLong(subjectId), Long.parseLong(studentId));
+		return new ResponseEntity<>("Delete successfully",HttpStatus.OK);
+		
+	}
+	
+	/**
+	 * Update fields in a SubjectStudent
+	 * 
+	 * @param tbd Object SubjectStudent with the new info
+	 * @return ResponseEntity with the SubjectStudentCuteDto modified
+	 */
+	@PutMapping
+	public ResponseEntity<SubjectStudentCuteDto> updateSubjectStudent(SubjectStudentCreationDto sscDto){
+		
+		return null;
+	}
+	
 }
