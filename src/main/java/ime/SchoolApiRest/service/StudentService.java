ad95cf09b574106d668612d1eff4ea4a6d6d25c5
IvanM <ivan.montes@outlook.com>
2023-09-06 17:34:42 +0200

    Developing Student service layer

--- src/main/java/ime/SchoolApiRest/service/StudentService.java
+++ src/main/java/ime/SchoolApiRest/service/StudentService.java
@@ -1,7 +1,29 @@
 package ime.SchoolApiRest.service;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import ime.SchoolApiRest.dto.StudentBasicCreationDto;
+import ime.SchoolApiRest.dto.StudentBasicDto;
+import ime.SchoolApiRest.dto.StudentDto;
 
 @Service
 public interface StudentService {
 
+	List<StudentDto>getAllStudent();
+	
+	StudentDto getStudentDtoById(Long id);
+	
+	void deleteStudentDtoById(Long id);
+	
+	StudentBasicDto createStudent(StudentBasicCreationDto student);
+	
+	StudentBasicDto updateStudent(StudentBasicDto student);
+	
+	StudentDto addStudentToSubject(Long subjectId, Long studentId);
+	
+	StudentDto addStudentToSubjectWithMark(Long subjectId, Long studentId, Double averageGrade);
+	
+	StudentDto removeStudentToSubject(Long subjectId, Long studentId);
+	
 }
--- src/main/java/ime/SchoolApiRest/service/impl/StudentServiceImpl.java
+++ src/main/java/ime/SchoolApiRest/service/impl/StudentServiceImpl.java
@@ -1,10 +1,63 @@
 package ime.SchoolApiRest.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import ime.SchoolApiRest.dto.StudentBasicCreationDto;
+import ime.SchoolApiRest.dto.StudentBasicDto;
+import ime.SchoolApiRest.dto.StudentDto;
 import ime.SchoolApiRest.service.StudentService;
 
 @Service
 public class StudentServiceImpl implements StudentService {
+
+	@Override
+	public List<StudentDto> getAllStudent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StudentDto getStudentDtoById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteStudentDtoById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public StudentBasicDto createStudent(StudentBasicCreationDto student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StudentBasicDto updateStudent(StudentBasicDto student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StudentDto addStudentToSubject(Long subjectId, Long studentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StudentDto addStudentToSubjectWithMark(Long subjectId, Long studentId, Double averageGrade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StudentDto removeStudentToSubject(Long subjectId, Long studentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
