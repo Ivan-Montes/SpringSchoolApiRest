ddd81d3036c04b3a4560c9027215d98f716dd597
IvanM <ivan.montes@outlook.com>
2023-09-08 11:03:27 +0200

    Add 2 new mappers

--- src/main/java/ime/SchoolApiRest/mapper/StudentMapper.java
+++ src/main/java/ime/SchoolApiRest/mapper/StudentMapper.java
@@ -2,6 +2,8 @@ package ime.SchoolApiRest.mapper;
 
 import java.util.List;
 
+import ime.SchoolApiRest.dto.StudentBasicCreationDto;
+import ime.SchoolApiRest.dto.StudentBasicDto;
 import ime.SchoolApiRest.dto.StudentDto;
 import ime.SchoolApiRest.entity.Student;
 
@@ -23,6 +25,31 @@ public class StudentMapper {
 													.surname(student.getSurname())
 													.subjectStudents(SubjectStudentMapper.toSetSubjectStudentDto(student.getSubjects()))
 													.build();
+	}
+	
+	public static Student fromStudentBasicCreationDtoToStudent(StudentBasicCreationDto sbcDto) {
+		
+		Student student = new Student();
+		
+		if ( sbcDto != null ) {
+
+			student.setName(sbcDto.getName());
+			student.setName(sbcDto.getSurname());
+			
+		}		
+		
+		return student;
+		
+	}
+	
+	public static StudentBasicDto fromStudentToStudentBasicDto(Student student) {
+		
+		return student == null? new StudentBasicDto():StudentBasicDto.builder()
+													.studentId(student.getStudentId())
+													.name(student.getName())
+													.surname(student.getSurname())
+													.build();
+		
 	}
 
 }
