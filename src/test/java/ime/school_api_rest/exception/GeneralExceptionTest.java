ee868f946e4fbcbd277bbf019c6b518153d02ae9
IvanM <ivan.montes@outlook.com>
2023-10-17 16:17:18 +0200

    Create test for GeneralException class

---
+++ src/test/java/ime/school_api_rest/exception/GeneralExceptionTest.java
@@ -0,0 +1,22 @@
+package ime.school_api_rest.exception;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GeneralExceptionTest {
+
+	@Test
+	void generalException_GeneralException_ReturnObject() {
+
+		GeneralException generalException = new GeneralException("general","exception",33L);
+		
+		assertAll(
+				()->Assertions.assertThat(generalException.getName()).isEqualTo("general"),
+				()->Assertions.assertThat(generalException.getMessage()).isEqualTo("exception"),
+				()->Assertions.assertThat(generalException.getIdentifier()).isEqualTo(33L)
+				);		
+	}
+
+}
