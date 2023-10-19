f49ddaecd3e19fcff56e1b226d5f771b9d2db8d5
IvanM <ivan.montes@outlook.com>
2023-10-19 16:19:59 +0200

    Complete test to cover more exceptions

--- src/test/java/ime/school_api_rest/exception/GlobalExceptionHandlerTest.java
+++ src/test/java/ime/school_api_rest/exception/GlobalExceptionHandlerTest.java
@@ -6,20 +6,90 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import jakarta.validation.ConstraintViolationException;
 
 class GlobalExceptionHandlerTest {
 
+	private final long treintaYTres = 33L;
+	
 	@Test
 	void globalExceptionHandler_generalException_ReturnResponseEntity() {
 		
 		GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
-		GeneralException generalException = new GeneralException("general","exception",33L);
-		ResponseEntity<String> response = globalExceptionHandler.generalException(generalException);
+		GeneralException ex = new GeneralException("general","exception", treintaYTres);
+		ResponseEntity<String> response = globalExceptionHandler.generalException(ex);
 		
 		assertAll(
 				()->Assertions.assertThat(response).isNotNull(),
 				()->Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.EXPECTATION_FAILED)
 				);
 	}
+	
+	@Test
+	void globalExceptionHandler_resourceNotFoundException_ReturnResponseEntity() {
+		
+		GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+		ResourceNotFoundException ex = new ResourceNotFoundException(treintaYTres);
+		ResponseEntity<String> response = globalExceptionHandler.resourceNotFoundException(ex);
+		
+		assertAll(
+				()->Assertions.assertThat(response).isNotNull(),
+				()->Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND)
+				);
+	}
+	
+	@Test
+	void globalExceptionHandler_elementAssociatedException_ReturnResponseEntity() {
+		
+		GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+		GeneralException ex = new SubjectAssociatedException(treintaYTres);
+		ResponseEntity<String> response = globalExceptionHandler.elementAssociatedException(ex);
+		
+		assertAll(
+				()->Assertions.assertThat(response).isNotNull(),
+				()->Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.PRECONDITION_REQUIRED)
+				);
+	}
 
+	@Test
+	void globalExceptionHandler_methodArgumentTypeMismatchException_ReturnResponseEntity() {
+		
+		GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+		MethodArgumentTypeMismatchException ex = new MethodArgumentTypeMismatchException(null, null, null, null, new Exception());
+		ResponseEntity<String> response = globalExceptionHandler.methodArgumentTypeMismatchException(ex);
+		
+		assertAll(
+				()->Assertions.assertThat(response).isNotNull(),
+				()->Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST)
+				);
+	}
+
+	@Test
+	void globalExceptionHandler_badCredentialsException_ReturnResponseEntity() {
+		
+		GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+		BadCredentialsException ex = new BadCredentialsException(null);
+		ResponseEntity<String> response = globalExceptionHandler.badCredentialsException(ex);
+		
+		assertAll(
+				()->Assertions.assertThat(response).isNotNull(),
+				()->Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.PROXY_AUTHENTICATION_REQUIRED)
+				);
+	}
+
+	@Test
+	void globalExceptionHandler_jakartaValidationConstraintViolationException_ReturnResponseEntity() {
+		
+		GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+		ConstraintViolationException ex = new ConstraintViolationException(null);
+		ResponseEntity<String> response = globalExceptionHandler.jakartaValidationConstraintViolationException(ex);
+		
+		assertAll(
+				()->Assertions.assertThat(response).isNotNull(),
+				()->Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST)
+				);
+	}
 }
